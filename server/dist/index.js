079f96d40deabb61b1b54232eb06bfbd88f33a1d
Ritik Kumar Pathak <phritik06@gmail.com>
2024-11-14 02:43:37 +0530

    build fixes

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,9 +3,7 @@ import morgan from "morgan";
 import connectDatabase from "./config/db";
 import contactRoute from "./routes/contactroutes";
 import cors from "cors";
-import path from "path";
 const app = express();
-const __dirname = path.resolve();
 const port = process.env.PORT || 8000;
 // Configs
 app.use(express.json());
@@ -13,11 +11,6 @@ app.use(morgan("common"));
 app.use(cors());
 connectDatabase();
 app.use("/api", contactRoute);
-app.use(express.static(path.join(__dirname, '../client/dist')));
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 app.get("/", async (req, res) => {
     res.send("Server working fine");
 });
